ae13d066c41fe2891ddf178ddccc9170662ad047
Marco Baroncini <spartaco4404@yahoo.it>
2018-07-23 13:53:11 +0200

    Il mio primo sito web

--- index.php
+++ index.php
@@ -1,2 +1,18 @@
 <?php
-echo uniqid();
+
+$color = '#' . substr( md5( time() ) , 0, 6 ) ;
+ob_start();
+
+?>
+<html>
+<body style='background-color:<?php echo $color ?>'>
+<p >Testo di prova</p>
+</body>
+</html>
+<?php
+
+$html = ob_get_clean();
+
+
+
+echo $html;
